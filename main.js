d7673864e7bd221454580bec592715d8e02bdd01
mfreire1996 <mfreire1996@gmail.com>
2020-11-14 16:23:01 -0500

    First commit

---
+++ main.js
@@ -0,0 +1,99 @@
+// vat tl = new TimeLineMax({ paused: true});
+// TweenMax.from("#title", 0.5 {
+//     opactity: 0,
+//     y: 10,
+//     delay: 1
+// });
+
+// var t1 = new TimelineMax({ paused: true });
+// TweenMax.from(".logo", 0.5, {
+//   opacity: 0,
+//   y: 10,
+//   delay: 1
+// });
+
+// var t1 = new TimelineMax({ paused: true });
+// TweenMax.from(".home-btn", 0.5, {
+//   opacity: 0,
+//   y: 10,
+//   delay: 1
+// });
+
+// var t1 = new TimelineMax({ paused: true });
+// TweenMax.from(".store-btn", 0.5, {
+//   opacity: 0,
+//   y: 10,
+//   delay: 1
+// });
+
+// TweenMax.from(".menu", 0.5, {
+//   opacity: 0,
+//   y: 10,
+//   delay: 1.2
+// });
+// TweenMax.from(".title", 3, {
+//   opacity: 0,
+//   delay: 1.4
+// });
+// TweenMax.from(".tagline", 3, {
+//   opacity: 0,
+//   delay: 1.6
+// });
+// TweenMax.from(".moreitems", 0.5, {
+//   opacity: 0,
+//   y: 4,
+//   delay: 1.8
+// });
+// t1.to(".items", 1, {
+//   autoAlpha: 1
+// });
+// t1.from(
+//   ".one",
+//   0.5,
+//   {
+//     opacity: 0,
+//     y: 20,
+//     delay: 0.1
+//   },
+//   "-=1"
+// );
+// t1.from(
+//   ".two",
+//   0.5,
+//   {
+//     opacity: 0,
+//     y: 20,
+//     delay: 0.3
+//   },
+//   "-=1"
+// );
+// t1.from(
+//   ".three",
+//   0.5,
+//   {
+//     opacity: 0,
+//     y: 20,
+//     delay: 0.5
+//   },
+//   "-=1"
+// );
+// t1.from(
+//   ".four",
+//   0.5,
+//   {
+//     opacity: 0,
+//     y: 20,
+//     delay: 0.7
+//   },
+//   "-=1"
+// );
+// t1.reverse();
+// $(document).on("click", ".moreitems", function() {
+//   t1.reversed(!t1.reversed());
+// });
+// $(document).on("click", "a", function() {
+//   t1.reversed(!t1.reversed());
+// });
+// $(document).on("click", ".close-btn a", function() {
+//   t1.reversed(!t1.reversed());
+// });
